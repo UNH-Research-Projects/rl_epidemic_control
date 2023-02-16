9b8f0380d10d03e31dd635e392f8fb923441c522
Merishna <merishna97@gmail.com>
2023-02-16 14:17:21 -0500

    Updated functions for plotting

--- pandemic_env/environment.py
+++ pandemic_env/environment.py
@@ -43,14 +43,14 @@ class PandemicEnv(gym.Env):
         lockdown_cost,
         transmission_rate,
         sensitivity,
-        reward_type,
+        reward_factor,
         plot_title=None,
         train=True
     ):
         # super(PandemicEnv, self).__init__()
         self.m = m
         self.n = n
-        self.reward_type = reward_type
+        self.reward_factor = reward_factor
         self.plot_title = plot_title
         # Define action and observation space
         self.action_space = spaces.Discrete(2)
@@ -77,7 +77,7 @@ class PandemicEnv(gym.Env):
         self.infected_num_list, self.vaccinated_num_list, self.recovered_num_list = [], [], []
         self.reward_list, self.actions_taken = [], []
         self.avg_infected_epi, self.avg_vaccinated_epi, self.avg_recovered_epi = [], [], []
-        self.train = train
+        self.train = train # boolean to determine training or testing
 
     def step(self, action):
         """Execute one time step within the environment.
@@ -99,7 +99,7 @@ class PandemicEnv(gym.Env):
         self.pandemic_length += iteration
 
         # actions
-        self.actions_taken.append(action)
+        # self.actions_taken.append(action)
         num_infected = self.players_lattice.count_num_strategy(2)
 
         # print("Infections for step {}: {} ".format(self.pandemic_length, num_infected))
@@ -107,67 +107,19 @@ class PandemicEnv(gym.Env):
         self.vaccinated_num_list.append(self.players_lattice.count_num_strategy(1))
         self.recovered_num_list.append(self.players_lattice.count_num_strategy(3))
 
-        reward = self.players_lattice.calc_reward(contact_rate, self.pandemic_length, self.reward_type)
+        reward = self.players_lattice.calc_reward(contact_rate, self.pandemic_length, self.reward_factor)
         self.reward_list.append(reward)
 
         # if self.pandemic_length >= 100:
         # if self.players_lattice.count_num_strategy(2) <= 0.01*(self.m*self.n):
         if num_infected <= 0:
-            # self.avg_infected_epi.append(sum(self.infected_num_list)) #/len(self.infected_num_list))
-            # self.avg_vaccinated_epi.append(sum(self.vaccinated_num_list)) #/len(self.vaccinated_num_list))
-            # self.avg_recovered_epi.append(sum(self.recovered_num_list)) #/len(self.recovered_num_list))
-            # self.avg_infected_epi.append(self.players_lattice.count_num_strategy(2))
-            # self.avg_vaccinated_epi.append(self.players_lattice.count_num_strategy(1))
-            # self.avg_recovered_epi.append(self.players_lattice.count_num_strategy(3))
+
             if self.plot_title is not None:
-
-                fig, ax = plt.subplots()
-                ax.plot(self.infected_num_list, color="red")
-                ax.plot(self.vaccinated_num_list, color="blue")
-                ax.plot(self.recovered_num_list, color="green")
-                ax.set_xlabel("Length of the pandemic")
-                ax.set_ylabel("Number of individuals")
-                ax.legend(['Infected', 'Vaccinated', 'Recovered'])
-                if self.train:
-                    ax.set_title("Change in total number of individuals \n for training", fontdict={'size': 10})
-                else:
-                    ax.set_title("Change in total number of individuals \n for " + str(self.plot_title), fontdict={'size': 10})
-                    
-                plt.show()
-                fig.savefig("change_plot_" + str(self.pandemic_length)+ ".png", dpi=400)
-
-                fig2, axe = plt.subplots()
-                axe.plot(self.reward_list, color="green")
-                axe.set_xlabel("Length of the pandemic")
-                axe.set_ylabel("Reward")
-                # ax.legend(['Infected', 'Vaccinated', 'Recovered'])
-                if self.train:
-                    axe.set_title("Model Reward for training", fontdict={'size': 10})
-
-                else:
-                    axe.set_title("Model Reward for "+ str(self.plot_title), fontdict={'size': 10})
-        
-                plt.show()
-                fig2.savefig("reward_alternative_" + str(self.pandemic_length)+ ".png", dpi=400)
-
-                fig3, ax3 = plt.subplots()
-                colors = ['red' if i == 1 else 'green' for i in self.actions_taken]
-
-                ax3.scatter(np.arange(1, len(self.actions_taken)+1), self.actions_taken, c=colors)
-                axe.set_xlabel("Length of the pandemic")
-                axe.set_ylabel("Action")
+                self.players_lattice.plot_episode_changes(self.plot_title, self.pandemic_length, self.actions_taken) 
                 
-                if self.train:
-                    ax3.set_title("Model actions for training", fontdict={'size': 10})
-
-                else:
-                    ax3.set_title("Model actions for "+ str(self.plot_title), fontdict={'size': 10})
-
-                plt.show()
-                fig3.savefig("actions" + str(self.pandemic_length)+ ".png", dpi=400)
-
-                self.infected_num_list, self.vaccinated_num_list, self.recovered_num_list = [], [], []
-                self.reward_list = []   
+            # reset list values    
+            self.infected_num_list, self.vaccinated_num_list, self.recovered_num_list = [], [], []
+            self.reward_list = []  
 
             done = True
         else:
